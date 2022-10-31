ae5f494407872d134b5f71a333ca5c1140683bb9
aline_pencreach <aline.pencreach@gmail.com>
2022-10-31 15:47:15 +0100

    calculator front ok

---
+++ app.js
@@ -0,0 +1,16 @@
+// const numberButtons = [...document.querySelectorAll('[data-number]')];
+// const dataNumber = numberButtons.map(numberButtons => numberButtons.dataset.number);
+// console.log(numberButtons);
+// console.log(dataNumber);
+
+
+
+
+// const operationButtons = document.querySelectorAll('[data-operation]');
+// const equalsButton = document.querySelector('[data-equals]');
+// const deleteButton = document.querySelector('[data-delete]');
+// const allClearButton = document.querySelector('[data-all-clear]');
+// const calculationDisplay = document.querySelector(".calculation");
+// const resultDisplay = document.querySelector(".result");
+
+
